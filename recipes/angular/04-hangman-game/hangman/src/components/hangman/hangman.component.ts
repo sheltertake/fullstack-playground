b5157aaafd4084832537e33f61a5f81513b6d689
LorenzoLombardo99 <lorenzoloccio99@gmail.com>
2022-02-21 12:37:40 +0100

    04 hangman game finished

--- recipes/angular/04-hangman-game/hangman/src/components/hangman/hangman.component.ts
+++ recipes/angular/04-hangman-game/hangman/src/components/hangman/hangman.component.ts
@@ -1,52 +1,62 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HangmanService } from 'src/service/hangman.service';
+
 
 @Component({
   selector: 'app-hangman',
   templateUrl: './hangman.component.html',
-  styleUrls: ['./hangman.component.scss']
+  styleUrls: ['./hangman.component.scss'],
 })
 export class HangmanComponent implements OnInit {
-
-  question:string ='';
+  question: string = '';
   questions: string[] = [];
-  guesses:string[] = [];
-  category: string= '';
-
-
-  
-  constructor(private hangmanService: HangmanService) { }
+  guesses: string[] = [];
+  category: string = '';
+  restartGameBtnShown = false;
+  constructor(
+    private hangmanService: HangmanService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
-this.hangmanService.getQuestion().subscribe(
-(response) =>  {
-  this.questions= response.items;
-  this.category= response.category;
-  this.pickNewQuestion();
-})
+    let jsonPath;
+    const url = this.location.path();
+    if (url.includes('jsonPath')) {
+      jsonPath = url.split('jsonPath=')[1];
+    }
+    this.hangmanService.getQuestions(jsonPath).subscribe((response) => {
+      this.questions = response.items;
+      this.category = response.category;
+      this.pickNewQuestion();
+    });
   }
 
-  
-  guess(letter: string){
-    if (!letter || this.guesses.includes(letter)){
+  guess(letter: string) {
+    if (!letter || this.guesses.includes(letter)) {
       return;
     }
-    this.guesses= [...this.guesses,letter];
+    this.guesses = [...this.guesses, letter];
   }
 
-  dummyClick(){
-    const key = prompt('enter a key') || '';
+  dummyClick() {
+    const key = prompt('Enter a key') || '';
     this.guess(key);
   }
 
-  reset(){
-    this.guesses =[];
+  reset() {
+    this.guesses = [];
     this.pickNewQuestion();
+    this.restartGameBtnShown = false;
   }
+
   pickNewQuestion() {
-    const randomIndex = Math.floor(Math.random()* this.questions.length);
+    const randomIndex = Math.floor(Math.random() * this.questions.length);
     this.question = this.questions[randomIndex];
     console.log(this.question);
   }
 
+  onGameFinished() {
+    this.restartGameBtnShown = true;
+  }
 }
