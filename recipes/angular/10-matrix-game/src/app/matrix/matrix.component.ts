29b90ea05905a2d5cae592f3b749882e68ee61d5
LorenzoLombardo99 <lorenzoloccio99@gmail.com>
2022-02-24 17:41:15 +0100

    10-matrix game finished

--- recipes/angular/10-matrix-game/src/app/matrix/matrix.component.ts
+++ recipes/angular/10-matrix-game/src/app/matrix/matrix.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { from } from 'rxjs';
 
 type Cell = boolean | null;
 
@@ -60,8 +61,8 @@ export class MatrixComponent implements OnInit {
     window.addEventListener('DOMContentLoaded', (event) => {
       let audio = document.querySelector('audio');
       if (audio) {
-        audio.volume = 0.2;
-        audio.play().then((c) => {});
+        audio.volume = 0.5;
+        audio.play();
       }
     });
   }
@@ -81,8 +82,8 @@ export class MatrixComponent implements OnInit {
 
     this.enemies = this.size;
     this.matrix = this.buildMatrix();
-    this.matrix[this.x][this.y] = true;
-    for (let i = 0; i < 3; i++) {
+    this.matrix[this.y][this.x] = true;
+    for (let i = 0; i < this.enemies; i++) {
       this.randX = this.randomObstacle();
       this.randY = this.randomObstacle();
 
@@ -98,115 +99,208 @@ export class MatrixComponent implements OnInit {
     }
   }
 
+  chekMatrix(): number {
+    let i = 0;
+    let counter = 0;
+    while (i < this.matrix.length) {
+      let j = 0;
+      while (j < this.matrix.length) {
+        if (this.matrix[i][j] === null) {
+          counter++;
+        }
+        j++;
+      }
+      i++;
+    }
+    return counter;
+  }
+
   setEnemy(x: number, y: number) {
     this.randX = x;
     this.randY = y;
     this.matrix[x][y] = null;
   }
-  moveEnemy(x: number, y: number) {
-    // var elems = document.querySelectorAll<HTMLElement>('.null-card');
+  moveEnemy(caller: string) {
 
+
+    this.enemies = this.chekMatrix();
+    if(this.chekMatrix() === 0)
+    {
+      alert('Winner');
+    }
+
+
+
+    console.log('move');
+    if (caller === 'right') {
+      if (
+        this.matrix[this.y][this.x + 1] === null &&
+        this.x + 2 <= this.matrix.length
+      ) {
+        this.matrix[this.y][this.x + 1] = false;
+
+        if (this.y === this.matrix.length - 1) {
+          console.log('dfgd');
+        } else {
+          this.matrix[this.y + 1][this.x + 1] = null;
+        }
+      }
+    } else {
+    }
+
+    if (caller === 'left') {
+      if (this.matrix[this.y][this.x - 1] === null && this.x - 2 <= 0) {
+        this.matrix[this.y][this.x - 1] = false;
+        console.log('x' + this.x);
+
+        if (this.y === this.matrix.length - 1) {
+          console.log('dfgd');
+        } else {
+          this.matrix[this.y + 1][this.x - 1] = null;
+        }
+      }
+    } else {
+    }
+
+    if (caller === 'down') {
+      // this.matrix[this.y-8][this.x]=true;
+      console.log('y' + this.y);
+      if(this.y === this.matrix.length-1){
+        return;
+      }
+
+        if (this.matrix[this.y + 1][this.x] === null) {
+          this.matrix[this.y + 1][this.x] = false;
+
+          if(this.x===this.matrix.length-1){
+
+          } else {
+            this.matrix[this.y + 1][this.x + 1] = null;
+          }
+
+        }
+
+    } else {
+    }
+
+    if (caller === 'up') {
+      // this.matrix[this.y+8][this.x]=true;
+      console.log('y' + this.y);
+if(this.y === 0){
+  return;
+}
+        if (this.matrix[this.y - 1][this.x] === null) {
+          this.matrix[this.y - 1][this.x] = false;
+          this.matrix[this.y - 1][this.x - 1] = null;
+        }
+
+    } else {
+    }
+
+    // for (let i = 0; i < this.matrix.length; i++) {
+
+    //   for (let j = 0; j < this.matrix.length; j++) {
+    //     if (this.matrix[i][j] === null) {
+
+    //       if ( this.matrix[i + 1][j] === null && i + 1 != this.matrix.length - 1) {
+    //         this.matrix[i][j] = false;
+    //        this.matrix[i+1][j] = null;
+    //        return;
+    //       } else {
+    //         console.log("niente");
+
+    //       }
+    //     }
+    //   }
+    // }
+
+    // var elems = document.querySelectorAll<HTMLElement>('.null-card');
     // var index = 0,
     //   length = elems.length;
     // for (; index < length; index++) {
     //   const element = elems[index];
     //   console.log(element);
-
     // }
-    let i = 0;
-    let randomMov = this.randomMove();
-    while (i < this.matrix.length - 1) {
-      let j = 0;
-      while (j < this.matrix.length - 1) {
-        switch (randomMov) {
-          case 0:
-            console.log('0');
-            if (this.matrix[i][j] === null && this.matrix[i + 1][j] != true) {
-              if (i + 1 < this.matrix.length && this.matrix[0][j] === false) {
-                this.matrix[i][j] = false;
-                this.matrix[0][j] = null;
-              }
-
-              this.matrix[i][j] = false;
-              this.matrix[i + 1][j] = null;
-              return;
-            } else {
-            }
-
-            break;
-          case 1:
-            if (
-              this.matrix[i][j] === null &&
-              this.matrix[i + 1][j + 1] != true
-            ) {
-              if (
-                i + 1 < this.matrix.length &&
-                j + 1 < this.matrix.length - 1 &&
-                this.matrix[0][this.matrix.length - 1] === false
-              ) {
-                this.matrix[i][j] = false;
-                this.matrix[0][this.matrix.length - 1] = null;
-              }
-              this.matrix[i][j] = false;
-              this.matrix[i + 1][j + 1] = null;
-              return;
-            } else {
-            }
-            console.log('1');
-            break;
-          case 2:
-            if (this.matrix[i][j] === null && this.matrix[i][j + 1] != true) {
-              if (
-                j + 1 < this.matrix.length &&
-                this.matrix[i][this.matrix.length] === false
-              ) {
-                this.matrix[i][j] = false;
-                this.matrix[i][this.matrix.length] = null;
-              }
-              this.matrix[i][j] = false;
-              this.matrix[i][j + 1] = null;
-              return;
-            } else {
-            }
-            console.log('2');
-
-            break;
-          case 3:
-            console.log('3');
-            if (this.matrix[i][j] === null && this.matrix[i + 2][j] != true) {
-              if (
-                i + 2 > this.matrix.length &&
-                this.matrix[this.matrix.length][j] === false
-              ) {
-                this.matrix[i][j] = false;
-                this.matrix[this.matrix.length][j] = null;
-              }
-              this.matrix[i][j] = false;
-              this.matrix[i + 2][j] = null;
-              return;
-            } else {
-            }
-            break;
-        }
-
-        j++;
-      }
-      i++;
-    }
-    // if (x+2<this.matrix.length-1 && this.matrix[x + 2][y] === null) {
-    //   this.matrix[x + 2][y] = false;
-
-    //   if (x+3<this.matrix.length-1 && this.matrix[x + 3][y] != null) {
-    //     this.matrix[x + 3][y] = null;
-    //     console.log('ok');
-    //   } else {
-    //     console.log('d');
-    //   }
-    // } else {
-    //   console.log("fff");
-
+    // let i = 0;
+    // while (i < this.matrix.length - 1) {
+    //   let j = 0;
+    //   while (j < this.matrix.length - 1) {
+    //     // let randomMov = this.randomMove();
+    //     if(this.chekMatrix()<this.enemies){
+    //     }
+    // this.matrix[i][j] = false;
+    // switch (randomMov) {
+    //   case 0:
+    //     console.log('0');
+    //     if (this.matrix[i][j] === null) {
+    //       if (i + 1 < this.matrix.length-1 && this.matrix[0][j] === false) {
+    //         this.matrix[i][j] = false;
+    //         this.matrix[0][j] = null;
+    //         return;
+    //       }
+    //       this.matrix[i][j] = false;
+    //       this.matrix[i + 1][j] = null;
+    //       return;
+    //     }
+    //     break;
+    //   case 1:
+    //     if (this.matrix[i][j] === null) {
+    //       if (i + 1 < this.matrix.length-1 && this.matrix[0][j] === false) {
+    //         this.matrix[i][j] = false;
+    //         this.matrix[0][j] = null;
+    //         return;
+    //       }
+    //       this.matrix[i][j] = false;
+    //       this.matrix[i + 1][j] = null;
+    //       return;
+    //     }
+    //     console.log('1');
+    //     break;
+    //   case 2:
+    //     if (this.matrix[i][j] === null) {
+    //       if (i + 1 < this.matrix.length-1 && this.matrix[0][j] === false) {
+    //         this.matrix[i][j] = false;
+    //         this.matrix[0][j] = null;
+    //         return;
+    //       }
+    //       this.matrix[i][j] = false;
+    //       this.matrix[i + 1][j] = null;
+    //       return;
+    //     }
+    //     console.log('2');
+    //     break;
+    //   case 3:
+    //     console.log('3');
+    //     if (this.matrix[i][j] === null) {
+    //       if (i + 1 < this.matrix.length-1 && this.matrix[0][j] === false) {
+    //         this.matrix[i][j] = false;
+    //         this.matrix[0][j] = null;
+    //         return;
+    //       }
+    //       this.matrix[i][j] = false;
+    //       this.matrix[i + 1][j] = null;
+    //       return;
+    //     }
+    //     break;
     // }
+    //   j++;
+    // }
+    // i++;
   }
+  // if (x+2<this.matrix.length-1 && this.matrix[x + 2][y] === null) {
+  //   this.matrix[x + 2][y] = false;
+
+  //   if (x+3<this.matrix.length-1 && this.matrix[x + 3][y] != null) {
+  //     this.matrix[x + 3][y] = null;
+  //     console.log('ok');
+  //   } else {
+  //     console.log('d');
+  //   }
+  // } else {
+  //   console.log("fff");
+
+  // }
+
   randomMove(): number {
     return Math.floor(Math.random() * 4);
   }
@@ -239,7 +333,8 @@ export class MatrixComponent implements OnInit {
   }
   goRight(): void {
     this.upImg('right');
-    this.moveEnemy(this.x, this.y);
+    this.moveEnemy('right');
+    this.enemies = this.chekMatrix();
     if (this.x < this.matrix.length - 1) {
       if (this.matrix[this.y][this.x + 1] === null) {
         this.findObstacle();
@@ -261,7 +356,8 @@ export class MatrixComponent implements OnInit {
   }
   goLeft(): void {
     this.upImg('left');
-    this.moveEnemy(this.x, this.y);
+    this.moveEnemy('left');
+    this.enemies = this.chekMatrix();
     if (this.x > 0) {
       if (this.matrix[this.y][this.x - 1] == null) {
         this.findObstacle();
@@ -282,10 +378,12 @@ export class MatrixComponent implements OnInit {
   }
 
   goUp(): void {
+    this.moveEnemy('up');
     setTimeout(() => {
       this.upImg('up');
     }, 50);
-    this.moveEnemy(this.x, this.y);
+
+    this.enemies = this.chekMatrix();
     if (this.y > 0) {
       if (this.matrix[this.y - 1][this.x] === null) {
         this.findObstacle();
@@ -308,7 +406,8 @@ export class MatrixComponent implements OnInit {
     setTimeout(() => {
       this.upImg('down');
     }, 50);
-    this.moveEnemy(this.x, this.y);
+    this.moveEnemy('down');
+    this.enemies = this.chekMatrix();
     if (this.y < this.matrix.length - 1) {
       if (this.matrix[this.y + 1][this.x] === null) {
         this.findObstacle();
@@ -353,10 +452,7 @@ export class MatrixComponent implements OnInit {
   findObstacle() {
     console.log('Obstacle found');
     this.play();
-    if (this.enemies == 1) {
-      alert('Winner!');
-    }
-    this.enemies--;
+
   }
 
   play() {
