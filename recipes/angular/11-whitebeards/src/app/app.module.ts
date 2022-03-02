8bae74b2c6c0853603958a8022b3e7c12a3e382f
LorenzoLombardo99 <lorenzoloccio99@gmail.com>
2022-03-02 14:34:45 +0100

    best practices

---
+++ recipes/angular/11-whitebeards/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+
+import { appRoutes } from './routes'
+import { AppComponent }  from './app.component';
+import { NavBarComponent }  from './users/nav-bar.component';
+import { CatalogComponent } from "./catalog/catalog.component";
+import { RegisterComponent } from "./users/sign-in.component";
+import { SignInComponent } from "./users/sign-in.component";
+import { LoadingComponent } from "./components/loading-spinner.component";
+import { DataRepositoryService } from "./services/data-repository.service"
+import { AccountMenuComponent } from "./users/account-menu.component";
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CatalogComponent,
+    RegisterComponent,
+    SignInComponent,
+    LoadingComponent,
+    AccountMenuComponent
+  ],
+  providers: [ DataRepositoryService ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ recipes/angular/11-whitebeards/src/app/routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { RegisterComponent } from './users/sign-in.component';
+import { SignInComponent } from './users/sign-in.component';
+
+export const appRoutes : Routes = [
+  { path: 'catalog', component: CatalogComponent, },
+  { path: 'users/register', component: RegisterComponent, },
+  { path: 'users/sign-in', component: SignInComponent, },
+];
