1db1d18a3cfce064ed178439db9b6c32391ca12a
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2022-03-08 10:34:42 +0100

    tests

--- recipes/dotnet/12-db-dapper-tests/DapperCrudTests.cs
+++ recipes/dotnet/12-db-dapper-tests/DapperCrudTests.cs
@@ -1,5 +1,6 @@
 using Dapper;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 
@@ -9,20 +10,32 @@ public class DapperCrudTests
 {
     private string cs = @"Server=127.0.0.1;Initial Catalog=friends;Persist Security Info=True;User ID=SA;Password=yourStrong1234!Password;MultipleActiveResultSets=True;";
 
-    [SetUp]
-    public void Setup()
-    {
-
-    }
-
     [Test]
     public void SelectFriendsShouldReturnRecords()
     {
         using (var con = new SqlConnection(cs))
         {
             con.Open();
-            var friends = con.Query<Friend>("SELECT * FROM friends").ToList();
+            List<Friend> friends = con.Query<Friend>("SELECT * FROM friends").ToList();
             Assert.IsTrue(friends.Any());
+        }
+    }
+
+    [Test]
+    public void InsertNewFriend()
+    {
+        using (var con = new SqlConnection(cs))
+        {
+            con.Open();
+            var ret = con.Execute("INSERT INTO friends(Name) VALUES( @name )", new
+            {
+                name = "Lorenzo"
+            });
+            List<Friend> friends = con.Query<Friend>("SELECT * FROM friends").ToList();
+
+            Assert.IsTrue(friends.Any());
+
+            con.Execute("TRUNCATE TABLE friends");
         }
     }
 
---
+++ recipes/dotnet/12-db-dapper-tests/README.md
@@ -0,0 +1,7 @@
+# Getting started with dapper
+
+ - create a new nunit project
+
+```cmd
+ C:\GITHUB\fullstack-playground\recipes\dotnet> dotnet new nunit -n DapperTests -o 12-db-dapper-tests
+```
